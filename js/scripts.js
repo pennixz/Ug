601bf959be87033b1dae805d71b7c989f3aa7ebf
Espen Lillejord <espenlillejord@gmail.com>
2016-10-29 19:54:20 +0200

    0.05

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+function toggleNav() {
+    var mobileNav = document.getElementById('mobilenav');
+    mobileNav.className == "align-center hidden" ? mobileNav.className = "align-center showing": mobileNav.className ="align-center hidden";
+}
