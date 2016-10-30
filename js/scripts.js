db1021118cd2da27189c66f6dc6e9a42f32981a9
Espen Lillejord <espenlillejord@gmail.com>
2016-10-30 02:11:41 +0200

    0.09

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,43 @@ function toggleNav() {
         menu.innerHTML = "meny"
     }
 }
+
+
+function showMap() {
+    var mapDiv = document.getElementById('g_spot');
+    var text = document.getElementById('mapToggleText');
+    if(mapDiv.className == "hide") {
+        mapDiv.className = "show";
+        text.innerHTML = "skjul kart"
+        googleMaps();
+    }
+    else {
+        mapDiv.className = "hide"
+        text.innerHTML = "vis kart"
+    }
+}
+
+function googleMaps() {
+    var latLng = {
+        lat: 64.1199624
+        , lng: 11.4061595
+    };
+    var mapCanvas = document.getElementById('g_spot');
+    var mapOptions = {
+        center: new google.maps.LatLng(latLng)
+        , zoom: 10
+        , scrollwheel: false
+        , mapTypeId: 'roadmap'
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    var marker = new google.maps.Marker({
+        position: latLng
+        , map: map
+        , title: 'Ulstad Gård'
+    });
+    /* var directionsService = new google.maps.DirectionsService();
+     var directionsDisplay = new google.maps.DirectionsRenderer();
+     directionsDisplay.setMap(map);
+     directionsDisplay.setPanel8d*/
+
+}
